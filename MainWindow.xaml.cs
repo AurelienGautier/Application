614797b39e8cf82e8ec446aba119699a6461feb2
LaboTri-PC2 <LaboTri-PC2@DESKTOP-UE3F53I>
2024-04-03 14:16:23 +0200

    A txt file can now be parsed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +17,17 @@ namespace Application
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void Magic_Click(object sender, RoutedEventArgs e)
+        {
+            Parser parser = new Parser();
+            parser.ParseFile();
         }
     }
 }
---
+++ Parser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Application
+{
+
+    internal class Parser
+    {
+        private List<String[]> LinesParsed;
+
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32", SetLastError = true)]
+        public static extern void FreeConsole();
+
+        public Parser()
+        {
+            this.LinesParsed = new List<String[]>();
+
+            AllocConsole();
+        }
+
+        ~Parser() 
+        {
+            FreeConsole();
+        }
+
+        public void ParseFile()
+        {
+            StreamReader sr = new StreamReader("C:\\Users\\LaboTri-PC2\\Desktop\\dev\\test\\test.txt");
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Workbooks.Open("C:\\Users\\LaboTri-PC2\\Desktop\\dev\\test\\test.xlsx");
+
+            try
+            {
+                String line;
+                String[] words;
+                int nbLine = 55;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    words = line.Split(' ');
+                    words = (String[])words.Where((item, index) => item != "" && item != " ").ToArray();
+
+                    for(int i = 0; i < words.Length; i++) 
+                    {
+                        Console.WriteLine(words[i]);
+                    }
+
+                    this.LinesParsed.Add(words);
+
+                    nbLine++;
+                }
+
+                sr.Close();
+                excelApp.Workbooks.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Erreur : le fichier n'a pas pu être parsé correctement.");
+                sr.Close();
+                excelApp.Workbooks.Close();
+            }
+
+        }
+    }
+}
