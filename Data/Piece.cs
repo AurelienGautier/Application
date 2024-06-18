675ed278fa0acd19c25c13d469c832445f4988ac
LaboTri-PC2 <LaboTri-PC2@DESKTOP-UE3F53I>
2024-06-18 17:06:47 +0200

    La classe Piece est maintenant documentée correctement

--- Data/Piece.cs
+++ Data/Piece.cs
@@ -1,31 +1,30 @@
 ﻿namespace Application.Data
 {
+    /// <summary>
+    /// Represents a piece got from a source file.
+    /// </summary>
     public class Piece
     {
-        // Il y a une liste de données pour chaque plan de mesure pour les données de la pièce
         private readonly List<MeasurePlan> measurePlans;
-
         private readonly Header header;
 
         /*-------------------------------------------------------------------------*/
 
-        public Piece() 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Piece"/> class.
+        /// </summary>
+        public Piece()
         {
             this.measurePlans = new List<MeasurePlan>();
-
-            // Création de l'en-tête vide
             this.header = new Header();
         }
 
         /*-------------------------------------------------------------------------*/
 
-        /**
-         * GetLinesToWriteNumber
-         * 
-         * Retourne le nombre de lignes à prévoir dans le formulaire excel pour cette pièce
-         * return : int - Nombre de lignes à écrire
-         * 
-         */
+        /// <summary>
+        /// Gets the number of lines to write in the Excel form for this piece.
+        /// </summary>
+        /// <returns>The number of lines to write.</returns>
         public int GetLinesToWriteNumber()
         {
             int lineNb = 0;
@@ -40,27 +39,21 @@
 
         /*-------------------------------------------------------------------------*/
 
-        /**
-         * AddMeasurePlan
-         * 
-         * Ajoute un plan de mesure à la pièce
-         * measurePlan : String - Plan de mesure à ajouter
-         * 
-         */
-        public void AddMeasurePlan(String measurePlan)
+        /// <summary>
+        /// Adds a measure plan to the piece.
+        /// </summary>
+        /// <param name="measurePlan">The measure plan to add.</param>
+        public void AddMeasurePlan(string measurePlan)
         {
             this.measurePlans.Add(new MeasurePlan(measurePlan));
         }
 
         /*-------------------------------------------------------------------------*/
 
-        /**
-         * AddData
-         * 
-         * Ajoute une donnée à la pièce
-         * data : Data.Data - Donnée à ajouter
-         * 
-         */
+        /// <summary>
+        /// Adds data to the piece.
+        /// </summary>
+        /// <param name="data">The data to add.</param>
         public void AddData(Measure data)
         {
             if (this.measurePlans.Count == 0)
@@ -73,13 +66,10 @@
 
         /*-------------------------------------------------------------------------*/
 
-        /**
-         * GetMeasurePlans
-         * 
-         * Retourne la liste des plans de mesure utilisés pour mesurer la pièce
-         * return : List<String> - Liste des plans de mesure
-         * 
-         */
+        /// <summary>
+        /// Gets the list of measure plans used to measure the piece.
+        /// </summary>
+        /// <returns>The list of measure plans.</returns>
         public List<MeasurePlan> GetMeasurePlans()
         {
             return this.measurePlans;
@@ -87,11 +77,10 @@
 
         /*-------------------------------------------------------------------------*/
 
-        /**
-         * Crée l'en-tête à partir d'une chaîne de caractères
-         * text : String - L'en-tête récupérée de manière brute depuis le fichier à analyser
-         *
-         */
+        /// <summary>
+        /// Creates the header from a raw text.
+        /// </summary>
+        /// <param name="text">The raw header text.</param>
         public void CreateHeader(string text)
         {
             string[] lines = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
@@ -112,6 +101,10 @@
 
         /*-------------------------------------------------------------------------*/
 
+        /// <summary>
+        /// Gets the header of the piece.
+        /// </summary>
+        /// <returns>The header.</returns>
         public Header GetHeader()
         {
             return this.header;
